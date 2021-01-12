7d6d642002164080b2205e2a87da36548f6b2cca
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-01-12 17:15:00 +1100

    reCode loadHistory()

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -8,9 +8,11 @@ console.log(queryCurrentURL);
 console.log(queryForecastURL);
 // Array for search History?
 var searchHistory = [];
-console.log(searchHistory);
 
-// !! Remember document ready & prevent defaults !!
+// !! Remember document ready & prevent defaults == shorthand? !!
+// Function calls on page load
+loadHistory();
+searchAction();
 
 // function initialise() {
 //     loadHistory();
@@ -19,48 +21,35 @@ console.log(searchHistory);
 //     historyClick();
 //     weatherAPI();
 // }
-// // jQuery shorthand for document ready
-// $(function() {
-//     initialise();
-// });
 
+// Load localStorage data on refresh & create buttons for previous search(es)
 function loadHistory() {
     // Get savedCities from localStorage; set to city var (empty array) if none
     let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
-    savedCities.sort();
     console.log(savedCities);
-    // Loop through city in array, create button that runs weatherAPI when clicked
-    savedCities.forEach(function(city) {
-        let btnEl = document.createElement("button");
-        btnEl.textcontent = city;
-        btnEl.setAttribute("id", "btnItems");
-        let searchList = document.getElementById("searchHistory");
-        searchList.appendChild(btnEl);
-        $("#btnItems").click(function(event) {
-            event.preventDeault();
-            city = $(this).text().trim();
-            weatherAPI();
-        })
-    })
+    if (savedCities !== null) {
+        searchHistory = savedCities
+    }
+    renderBtns();
 }
 
-function searchAction() {
-    $("#searchForm").submit(function(event) {
-        event.preventDeault();
-        city = $("#searchBar").val();
-        if(city.length <= 0) {
-            alert("Please enter a city name.");
-        }
-        else {
-            let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
-            searchHistory = {
-                city: city
-            }
-            savedCities.push(searchHistory);
-            window.localStorage.setItem("savedHistory", JSON.stringify(savedCities));
-        };
-    })
-}
+// function searchAction() {
+//     $("#searchForm").submit(function(event) {
+//         event.preventDeault();
+//         city = $("#searchBar").val();
+//         if(city.length <= 0) {
+//             alert("Please enter a city name.");
+//         }
+//         else {
+//             let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
+//             searchHistory = {
+//                 city: city
+//             }
+//             savedCities.push(searchHistory);
+//             window.localStorage.setItem("savedHistory", JSON.stringify(savedCities));
+//         };
+//     })
+// }
 
 // // "click" eventListener for buttons
 //     // Search button runs weather & forecast functions
