08230a44d2654c503ba649dce18a044a3af02e27
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-01-14 08:36:28 +1100

    rethink structure

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,118 +1,77 @@
-var city = "";
-var apiKey = "&appid=471ac66dcee7336c8919fa741ccc6ad6";
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
-// Array for search History?
-var searchHistory = [];
+// Define global variables
+var searchBar = $("#searchBar");
+var searchBtn = $("#searchBtn");
+var searchHistory = $("#searchHistory");
+var apiKey = "471ac66dcee7336c8919fa741ccc6ad6";
+var weatherURL;
+var forecastURL;
+var savedSearch = [];
 
-// !! Remember document ready & prevent defaults == shorthand? !!
-// Function calls on page load
-loadHistory();
-listClick();
-searchAction();
-resetClick();
+// Load localStorage data on refresh 
+var loadStorage = localStorage.getItem("savedSearch");
+if (loadStorage != null) {
+    savedSearch = loadStorage.split(",");
+}
+// Variable for todays's date
+var today = new Date();
+var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
-// Load localStorage data on refresh & create buttons for previous search(es)
-function loadHistory() {
-    // Get savedCities from localStorage; set to city var (empty array) if none
-    let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
-    if (savedCities !== null) {
-        searchHistory = savedCities;
-    }
-    renderBtns();
+// Change format of date to Mo / Day / Yr
+function formatDates(data) {
+    var dateArray = data.split("-");
+    var dateFormat = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0];
+    return dateFormat
 }
 
-function renderBtns () {
-    $("#searchHistory").html("");
-    if (searchHistory === null) {
-        return;
-    }
-    let newCities = [...new Set(searchHistory)]
-    // Loop through array, create buttons for each city & run weatherAPI when clicked
-    for (let i = 0; i < newCities.length; i++) {
-        let cityName = newCities[i];
-        let btnEl = document.createElement("button");
-        btnEl.setAttribute("id", "btnItems");
-        btnEl.textContent = cityName;
-        console.log(cityName)
-        $("#searchHistory").append(btnEl);
-        listClick();
-    }
-}
+// function renderStorage() {
 
-function listClick() {
-    $("#btnItems").click(function(event) {
-        event.preventDefault();
-        city = $(this).text().trim();
-        $("#weatherHeader").text("Current Weather in " + city);
-        weatherAPI();
-    });
-}
+//     // Add value of searchBar to front of savedSearch array; splice values..?
+//     // Also place value at front of array if duplicate; maybe unshift??
 
-// Function for click event on searchBtn
-function searchAction() {
-    $("#searchBtn").click(function(event) {
-        event.preventDefault();
-        city = $("#searchBar").val().trim();
-        // Change to modal if time permits...
-        if(city.length <= 0) {
-            alert("Please enter a city name.");
-        }
-        else {
-            // Limit list to only show last 10 search results
-            // if (searchHistory.length > 10) {
-            //     searchHistory.shift();
-            // }
-            let nowDate = moment().format('LL');
-            console.log(nowDate)
-            $("#todayDate").html(nowDate);
-            $("#weatherHeader").text("Current Weather in " + city);
-            $("form").trigger("reset");
-            // ADD IF STATEMENT FOR INPUT !== A VALID CITY!
-            searchHistory.push(city);
-            window.localStorage.setItem("savedHistory", JSON.stringify(searchHistory))
-            savedCities = searchHistory;
-            renderBtns();
-            weatherAPI();
-        }
-    });
-}
+//     // Save to localStorage
 
-// Trash button deletes local storage data & resets search history
-function resetClick() {
-    $("#dltBtn").click(function(event) {
-        event.preventDefault();
-        window.localStorage.removeItem("savedHistory");
-        $("#searchHistory", ).remove();
-        $("#currentWeather").remove();
-    });
-    // FIX searchACTION AFTER CLEARING HISTORY!!!
-}
+//     // Creates historyList forLoop
 
-// currentWeather Function
-function weatherAPI() {
-    let queryCurrentURL = weatherURL + city + apiKey;
-    let queryForecastURL = forecastURL + city + apiKey;
-    console.log(queryCurrentURL);
-        
-    // Current Weather API Ajax call
-    $.get(queryCurrentURL, function(response) {
-        let temp = Math.round((( response.main.temp - 273.15) * 9/5 + 32))
-        console.log("The temperature in " + city + " is: " + temp);
-        
-        $("#temp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
-        $("#humidity").text("Humidity: " + response.main.humidity);
-        $("#wind").text("Wind Speed: " +  response.wind.speed);
-        // // Fix UV index
-        // $("#uv").text("UV Index: " +  ...);
-        $(".currentIcon").attr({"src": "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"});    
-    });
+//     // Click listner for list items
 
-    // $.get(queryForecastURL, function(response) {
-        
-    // });
-}      
+// }
 
-    // Temperature conversion
+// // searchBtn click listner; preventDefault
+// $("#searchBtn").click(function(event) {
 
-    // Append to HTML
+//     // Fxns to request get data
+
+// });
+
+// function getCurrent() {
+//     // Current Weather API call
+
+//         // Create object to store current weather data
+
+//         // Format object date
+
+//         // Call to get UV index 
+
+//             // Assign uvIntensity based on uvIndex number
+
+//             // Create card & append HTML elements for current weather data
+
+// }
+
+// function getForecast() {
+        // Array for looped forecast data?
+
+//      // Forecast Weather API call
+
+//         // forLoop to retrieve then push to forecast array
+
+//         // forLoop to format dates for array of objects
+
+//         // forLoop to create cards & append HTML elements for relevant forecast data
+
+// }
+
+// Run search on pressing Enter
+
+
+renderStorage();
