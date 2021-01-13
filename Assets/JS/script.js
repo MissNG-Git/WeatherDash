8d8405fd94fd5389a55f1220c41bb2384f78ed42
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-01-13 16:39:11 +1100

    fix renderBtns() error

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,6 @@
 var apiKey = "&appid=f6e64506bbf2ed97516451a25d139a01";
-var weatherURL = "api.openweathermap.org/data/2.5/weather?q="
-var forecastURL = "api.openweathermap.org/data/2.5/forecast?q="
+var weatherURL = "api.openweathermap.org/data/2.5/weather?q=";
+var forecastURL = "api.openweathermap.org/data/2.5/forecast?q=";
 var city = "";
 var queryCurrentURL = weatherURL + city + apiKey;
 var queryForecastURL = forecastURL + city + apiKey;
@@ -20,7 +20,6 @@ resetClick();
 function loadHistory() {
     // Get savedCities from localStorage; set to city var (empty array) if none
     let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
-    console.log(savedCities);
     if (savedCities !== null) {
         searchHistory = savedCities;
     }
@@ -32,13 +31,12 @@ function renderBtns () {
     if (searchHistory == null) {
         return;
     }
-    let cityList = [...new Set(searchHistory)];
     // Loop through array, create buttons for each city & run weatherAPI when clicked
-    for (let i = 0; i < cityList.length; i++) {
+    for (let i = 0; i < searchHistory.length; i++) {
         let btnEl = $("<button>");
         btnEl.attr("id", "btnItems");
-        btnEl.text(cityList[i].city.toUpperCase());
-        console.log(cityList[i].city)
+        btnEl.text(searchHistory[i].city.toUpperCase());
+        console.log(searchHistory[i].city);
         $("#searchHistory").append(btnEl);
         $("#btnItems").click(function(event) {
             event.preventDefault();
@@ -46,7 +44,6 @@ function renderBtns () {
             weatherAPI();
         });
     }
-    return(cityList);
 }
 
 // Function for click event on searchBtn
@@ -54,16 +51,15 @@ function searchAction() {
     $("#searchBtn").click(function(event) {
         event.preventDefault();
         city = $("#searchBar").val().trim();
-        console.log(city);
         // Change to modal if time permits...
         if(city.length <= 0) {
             alert("Please enter a city name.");
         }
-        // Limit list to only show last 10 search results
-        else if (searchHistory.length > 10) {
-            searchHistory.shift();
-        }
         else {
+            // Limit list to only show last 10 search results
+            if (searchHistory.length > 10) {
+                searchHistory.shift();
+            }
             let savedCities = JSON.parse(window.localStorage.getItem("savedHistory")) || searchHistory;
             searchHistory = {
                 city: city
@@ -71,10 +67,14 @@ function searchAction() {
             savedCities.push(searchHistory);
             window.localStorage.setItem("savedHistory", JSON.stringify(savedCities));
             renderBtns();
-            weatherAPI();
-        };
-    })
+            // weatherAPI();
+        }
+    });
 }
+
+// function historyClick() {
+
+// }
 
 // Trash button deletes local storage data & resets search history
 function resetClick() {
@@ -82,8 +82,8 @@ function resetClick() {
         event.preventDefault();
         window.localStorage.removeItem("savedHistory");
         $("#searchHistory").remove();
-    })
-};
+    });
+}
 
 // currentWeather Function
 function weatherAPI() {
@@ -92,16 +92,16 @@ function weatherAPI() {
     $.ajax({
         url: weatherURL,
         method: "GET"
-    })
+    });
     
     // Forecast API Ajax call
     $.ajax({
         url: forecastURL,
         method: "GET"
-    })
+    });
 
     // Temperature conversion
 
     // Append to HTML
 
-};
+}
